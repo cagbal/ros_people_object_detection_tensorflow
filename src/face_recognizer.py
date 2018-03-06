79fa93b742cb5f7f041f84dfbd8d81903673e5f6
cagbal <cagatay.odabasi@ipa.fraunhofer.de>
2018-03-06 17:35:51 +0100

    Added Face REcognition (in progress)

---
+++ src/face_recognizer.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+"""
+A ROS node to Finding Faces inside the detections of SSD network.
+
+Author:
+    Cagatay Odabasi -- cagatay.odabasi@ipa.fraunhofer.de
+"""
+
+import time
+
+import glob
+
+import rospy
+
+import message_filters
+
+import numpy as np
+
+import cv2
+
+from cv_bridge import CvBridge, CvBridgeError
+
+from sensor_msgs.msg import Image
+
+from cob_perception_msgs.msg import Detection, DetectionArray, Rect
+
+from cob_people_object_detection_tensorflow.detector import Detector
+
+from cob_people_object_detection_tensorflow import utils
+
+import face_recognition as fr
+
+import rospkg
+
+cd = rospack = rospkg.RosPack()
+
+cd = rospack.get_path('cob_people_object_detection_tensorflow')
+
+
+class FaceRecognitionNode(object):
+    """docstring for PeopleObjectDetectionNode."""
+    def __init__(self):
+        super(FaceRecognitionNode, self).__init__()
+
+        # init the node
+        rospy.init_node('face_recognition_node', anonymous=False)
+
+        # Get the parameters
+        (image_topic, detection_topic, output_topic) = self.get_parameters()
+
+        self._bridge = CvBridge()
+
+        # Advertise the result of Object Tracker
+        self.pub_out = rospy.Publisher(output_topic, \
+            DetectionArray, queue_size=1)
+
+        self.sub_detection = message_filters.Subscriber(detection_topic, \
+            DetectionArray)
+        self.sub_image = message_filters.Subscriber(image_topic, Image)
+
+        self.scaling_factor = 0.50
+
+        # Read the images from folder and create a database
+        self.database = self.initialize_database()
+
+        ts = message_filters.ApproximateTimeSynchronizer(\
+            [self.sub_detection, self.sub_image], 10, 0.1)
+
+        ts.registerCallback(self.detection_callback)
+
+        # spin
+        rospy.spin()
+
+    def get_parameters(self):
+        """
+        Gets the necessary parameters from parameter server
+
+        Args:
+
+        Returns:
+        (tuple) (camera_topic, detection_topic, output_topic)
+
+        """
+
+        camera_topic = rospy.get_param("~camera_topic")
+        detection_topic = rospy.get_param("~detection_topic")
+        output_topic = rospy.get_param("~output_topic")
+
+        return (camera_topic, detection_topic, output_topic)
+
+
+    def shutdown(self):
+        """
+        Shuts down the node
+        """
+        rospy.signal_shutdown("See ya!")
+
+    def detection_callback(self, detections, image):
+        """
+        Callback for RGB images and detections
+        Args:
+        detections: detections array message from cob package
+        image: sensor image
+
+        image: rgb frame in openCV format
+
+        """
+
+        cv_rgb = self._bridge.imgmsg_to_cv2(image, "passthrough")[:, :, ::-1]
+
+        cv_rgb = cv2.resize(cv_rgb, (0, 0), fx=self.scaling_factor, fy=self.scaling_factor)
+
+        cv_rgb=cv_rgb.astype(np.uint8)
+
+        for i, detection in enumerate(detections.detections):
+
+            print(type(cv_rgb))
+
+            if detection.label == "person":
+                x =  int(detection.mask.roi.x * self.scaling_factor)
+                y = int(detection.mask.roi.y * self.scaling_factor)
+                width = int(detection.mask.roi.width * self.scaling_factor)
+                height = int(detection.mask.roi.height * self.scaling_factor)
+                score = detection.score
+
+                # Crop detection image
+                detection_image = cv_rgb[x:x+width, y:y+height]
+
+                cv2.imshow("asdasd", detection_image)
+
+                cv2.waitKey(1)
+
+                try:
+                    face_locations = fr.face_locations(detection_image)
+
+                    face_features = fr.face_encodings(detection_image, \
+                        face_locations)
+
+                    for features, (top, right, bottom, left) in \
+                        zip(face_features, face_locations):
+                        matches = fr.compare_faces(self.database[0], features)
+
+                        left = left + x
+                        top = top + y
+                        right = right + x
+                        bottom = bottom + y
+
+                        if True in matches:
+                            ind = matches.index(True)
+
+                            cv2.rectangle(cv_rgb, (left, top), \
+                            (right, bottom), (0, 0, 255), 2)
+
+                            cv2.rectangle(cv_rgb, (left, bottom - 35), \
+                            (right, bottom), (0, 0, 255))
+
+                            font = cv2.FONT_HERSHEY_DUPLEX
+
+                            cv2.putText(cv_rgb, self.database[1][ind], \
+                            (left + 2, bottom - 2), \
+                            font, 1.0, (255, 255, 255), 1)
+
+                            cv2.imshow("asdasdljahsdlansd", cv_rgb)
+
+                            cv2.waitKey(1)
+
+                except Exception as e:
+                    pass
+
+
+
+
+    def initialize_database(self):
+        filenames = glob.glob(cd + '/people/*.png')
+
+        people_list = []
+        name_list = []
+
+        for f in filenames:
+            im = cv2.imread(f, 1)
+
+            cv2.imshow("Database Image", im)
+
+            cv2.waitKey(500)
+
+            im = im.astype(np.uint8)
+
+            people_list.append(fr.face_encodings(im)[0])
+
+            name_list.append(f.split('/')[-1])
+
+            cv2.destroyAllWindows()
+
+        return (people_list, name_list)
+
+def main():
+    """ main function
+    """
+    node = FaceRecognitionNode()
+
+if __name__ == '__main__':
+    main()
